269c3e15f57f5f9836151b1b519a0a7a5183b072
wojtekkurylo <wkurylo88@gmail.com>
2017-08-20 18:27:41 +0200

    Topic : Solved TODO
    * Added new Activity;
    * Modified AndroidManifest;

---
+++ app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    public TextView mDefaultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+
+        mDefaultTextView = (TextView) findViewById(R.id.tv_display);
+        mDefaultTextView.setText("default info from ChildActivity");
+    }
+}
